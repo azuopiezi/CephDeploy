c3945226d751a7620ac79c69a60174eccffbac44
shixingwe.n@163.com <shixingwe.n@163.com>
2020-09-29 17:25:15 +0800

    Ansible CephDeploy nopasswd ssh

---
+++ function/files/shell/CreateSshKey.sh
@@ -0,0 +1,7 @@
+if [ -e '/tmp/ceph_key_id.rsa' ]
+ then
+   echo "file exit"
+else
+  ssh-keygen -t rsa -b 2048 -f /tmp/ceph_key_id_rsa -q -N ''
+fi
+#if [ -e '/tmp/ceph_key_id_rsa' ] ;then echo 'file exit' ;else ssh-keygen -t rsa -b 2048 -f /tmp/ceph_key_id_rsa -q -N '';fi
